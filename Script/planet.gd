daa4d1d80b1f1dd319c3f4a63ba7335a0afa55bd
d4rkw3st <d4rkw3st.dw43@gmail.com>
2024-06-15 00:48:47 +0200

    Data folder with json file added, planet and world script added, placeholder textfile removed

---
+++ Script/planet.gd
@@ -0,0 +1,54 @@
+extends StaticBody2D
+
+var attract = false
+var force = 1
+var id = null
+var rotation_speed = 0
+var size = 1
+var sprite_path = ""
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func get_attract():
+	return self.attract
+
+func set_attract(new_attract):
+	self.attract = new_attract
+
+func get_force():
+	return self.force
+
+func set_force(new_force):
+	self.force = new_force
+
+func set_id(new_id):
+	self.id = new_id
+
+func get_id():
+	return self.id
+
+func get_rotation_speed():
+	return self.rotation_speed
+
+func set_rotation_speed(new_rotation_sprite):	
+	self.rotation_speed = new_rotation_sprite
+
+func get_size():
+	return self.size
+
+func set_size(new_size):
+	self.size = new_size
+	self.scale.x = self.size
+	self.scale.y = self.size
+	# adapt collision shape with size (shape circle)
+
+func get_sprite_path():
+	return self.sprite_path
+
+func set_sprite_path(new_sprite_path):
+	self.sprite_path = new_sprite_path
+	self.get_node("Sprite2D").texture = load(self.sprite_path)	
---
+++ Script/world.gd
@@ -0,0 +1,87 @@
+extends Node2D
+
+@export var planet_scene : PackedScene
+var planet_list = []
+var index = 0
+
+func _ready():
+	pass
+
+func _process(delta):
+	
+	check_input()
+	rotate_planets(delta)
+
+# Function to check input
+func check_input():
+	var mouse_pos = get_global_mouse_position()
+	
+	if Input.is_action_just_pressed("add_candy"):
+		adding_planet(mouse_pos,"PlanetCandy")
+
+	if Input.is_action_just_pressed("add_lemon"):
+		adding_planet(mouse_pos,"PlanetLemon")
+
+	if Input.is_action_just_pressed("add_marmelanete"):
+		adding_planet(mouse_pos,"Marmelanete")
+
+	if Input.is_action_just_pressed("increase_speed"):
+		if not planet_list.is_empty() :
+			for planet in planet_list:
+				planet.set_rotation_speed(planet.get_rotation_speed() + 1)
+
+	if Input.is_action_just_pressed("decrease_speed"):
+		if not planet_list.is_empty() :
+			for planet in planet_list:
+				planet.set_rotation_speed(planet.get_rotation_speed() - 1)
+	
+	if Input.is_action_just_pressed("increase_size"):
+		if not planet_list.is_empty() :
+			# check at mouse_pos if planet
+			# if yes, get id
+			# from list and id, change scale
+			pass
+
+	if Input.is_action_just_pressed("decrease_size"):
+		if not planet_list.is_empty() :
+			# check at mouse_pos if planet
+			# if yes, get id
+			# from list and id, change scale
+			pass
+
+# Function to rotate every planet
+func rotate_planets(delta):
+	
+	if not planet_list.is_empty() :
+		for planet in planet_list:
+			planet.rotate(delta * planet.get_rotation_speed())
+		
+
+# Function to add a planet
+func adding_planet(planet_position,planet_type):
+	#load json
+	var file = FileAccess.open("res://Datas/Planets.json", FileAccess.READ)
+	var data_dict = JSON.parse_string(file.get_as_text())
+	var planet_info = data_dict[planet_type]
+	#print(typeof(data)) # ENUM Variant, 27 = dictionnary
+	#print("planet info : ", planet_info)
+	#print("data : ", data)
+	#print("Type : ", planet_type)
+	
+	#spawn planet
+	var planet = planet_scene.instantiate()
+	
+	#extract info & apply properties
+	planet.set_attract(planet_info["attract"])
+	planet.set_force(planet_info["force"])
+	planet.set_id(index)
+	planet.position = planet_position
+	planet.set_rotation_speed(planet_info["rotation_speed"])
+	planet.set_size(planet_info["size"])
+	planet.set_sprite_path(planet_info["sprite_path"])	
+
+	#add planet to world and list
+	index = index + 1
+	self.add_child(planet)
+	planet_list.append(planet)
+	#print("id : ",index - 1)
