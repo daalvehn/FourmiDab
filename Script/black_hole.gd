3ac9fe948ed7a86898674eaef3326e5a164f93e4
Manwe <mathieu.jourdan.zola@gmail.com>
2024-06-16 15:10:17 +0200

    Add signal to blackHole prefab

--- Script/black_hole.gd
+++ Script/black_hole.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+signal player_in
+
 func _ready():
 	pass
-	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	var degrees_per_second = 360.0
+
 	$Sprite2D.rotate((delta / 100) * deg_to_rad(-degrees_per_second))
+
+
+func _on_area_2d_body_entered(body):
+	player_in.emit()
