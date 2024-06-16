24fa958128402b6c6fe31202fd0405ea37e0625a
Givr <lightpower@hotmail.fr>
2024-06-16 15:10:28 +0200

    update debris script so it works on PathFollow, or on Node2D with rigidbody

--- Script/PathDebris.gd
+++ Script/PathDebris.gd
@@ -1,13 +1,13 @@
-extends PathFollow2D
+extends Node2D
 @export var periodInSecond : float = 1
 @export var texture: Texture
-
 
 func _ready():
 	$Sprite2D.texture = texture
 
 func _process(delta):
-	self.progress += delta * 60 * 10 / periodInSecond
+	if self.get_child(0).get_parent() is PathFollow2D:
+		self.progress += delta * 60 * 10 / periodInSecond
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Fourmi":
