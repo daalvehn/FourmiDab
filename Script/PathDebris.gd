105a01bc523803f422eecca956e06250e9e36fa3
Manwe <mathieu.jourdan.zola@gmail.com>
2024-06-23 01:59:36 +0200

    Canards are orbitable again

--- Script/PathDebris.gd
+++ Script/PathDebris.gd
@@ -1,12 +1,11 @@
 extends Node2D
 @export	var rotation_period = 1
 @export var periodInSecond : float = 1
-
-var degrees_per_second = 360.0
+var degrees_per_second = 360
 
 func _process(delta):
-	var degrees_per_second = 360.0
-	rotate((delta / 2) * deg_to_rad(degrees_per_second)/ rotation_period) 
+	$Sprite2D.rotate((delta / 2) * deg_to_rad(degrees_per_second)/ rotation_period)
+	$Area2D.rotate((delta / 2) * deg_to_rad(degrees_per_second)/ rotation_period)
 	if self.get_child(0).get_parent() is PathFollow2D:
 		self.progress += delta * 60 * 10 / periodInSecond
 
